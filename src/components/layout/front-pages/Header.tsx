d03d09529304e71aa9ba52afad90bcb69e72715d
Syukur niyadi putra <syukur.putra@iconpln.co.id>
2025-09-14 15:57:16 +0700

    [Syukur] Verifikasi - Email

--- src/components/layout/front-pages/Header.tsx
+++ src/components/layout/front-pages/Header.tsx
@@ -53,14 +53,14 @@ const Header = ({ mode }: { mode: Mode }) => {
               <IconButton onClick={() => setIsDrawerOpen(true)} className='-mis-2'>
                 <i className='tabler-menu-2 text-textPrimary' />
               </IconButton>
-              <Link href='/front-pages/landing-page'>
+              <Link href='/h/landing-page'>
                 <Logo />
               </Link>
               <FrontMenu mode={mode} isDrawerOpen={isDrawerOpen} setIsDrawerOpen={setIsDrawerOpen} />
             </div>
           ) : (
             <div className='flex items-center gap-10'>
-              <Link href='/front-pages/landing-page'>
+              <Link href='/h/landing-page'>
                 <Logo />
               </Link>
               <FrontMenu mode={mode} isDrawerOpen={isDrawerOpen} setIsDrawerOpen={setIsDrawerOpen} />
