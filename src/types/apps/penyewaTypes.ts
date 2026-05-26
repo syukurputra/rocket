4a4afe027f96728fa7a0a2ee19cd7d3a0536187c
syukurniyadiputra <syukur.putra@iconpln.co.id>
2026-05-26 21:30:22 +0700

    [Syukur] Penyewa - Fixing penyewa

---
+++ src/types/apps/penyewaTypes.ts
@@ -0,0 +1,30 @@
+export type PenyewaClient = {
+  id: string
+  nama: string
+  email?: string | null
+  nomorTelepon?: string | null
+  status: string
+  periodeSewa?: string
+  mulaiSewa: string | Date
+  selesaiSewa: string | Date
+  asetId: string
+  ruanganId: string
+  aset?: {
+    id: string
+    nama: string
+    jenis: string
+  }
+  ruangan?: {
+    id: string
+    nama: string
+    status: string
+  }
+  createdBy?: {
+    id: string
+    username: string
+  }
+  updatedBy?: {
+    id: string
+    username: string
+  }
+}
